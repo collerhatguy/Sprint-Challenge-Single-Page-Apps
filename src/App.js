9d799b3422ec4729190b1de1d9a6a0f884c2e492
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-03-31 10:52:10 -0400

    api retrieval done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React from "react";
 import Header from "./components/Header.js";
 
 export default function App() {
+  const url = "https://rickandmortyapi.com/api/character";
+  async function getData(url) {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const json = await response.json();
+        return json;
+      } 
+    }
+    catch(err) {console.log(err)}
+  } 
+  const data = getData(url);
+  
   return (
     <main data-testid='app'>
       <Header />
