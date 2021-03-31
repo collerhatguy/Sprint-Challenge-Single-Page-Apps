ae514263ba5b8787e6fe559e5a99765ad9ae4ed1
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-03-31 11:11:29 -0400

    api integration is a bust

--- src/App.js
+++ src/App.js
@@ -2,22 +2,37 @@ import React from "react";
 import Header from "./components/Header.js";
 
 export default function App() {
+  // get data
   const url = "https://rickandmortyapi.com/api/character";
   async function getData(url) {
     try {
+      // get response
       const response = await fetch(url);
+      // if no problems then
       if (response.ok) {
+        // convert that from json
         const json = await response.json();
-        return json;
+        // get the results key
+        const releventData = await json.results;
+        console.log(releventData);
+        return releventData;
       } 
     }
     catch(err) {console.log(err)}
   } 
   const data = getData(url);
-  
+  const dataNames = data.then(data => data.map(character => {
+    return character.name;
+  }))
+  dataNames.then(names => console.log(names))
   return (
     <main data-testid='app'>
       <Header />
+      {dataNames.then(names => {
+        names.map(character => {
+          return <div>{character}</div>
+        })
+      })}
     </main>
   );
 }
